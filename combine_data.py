086616f08adcb13c4a18616a1e554612f1e747f5
AdamSpannbauer <spannbaueradam@gmail.com>
2020-05-29 07:42:49 -0400

    fix bug in combine data

--- combine_data.py
+++ combine_data.py
@@ -2,6 +2,6 @@ import glob
 import pandas as pd
 
 # Read and combine all relevant csvs
-csv_paths = glob.glob("data/*.csv")
+csv_paths = sorted(glob.glob("data/2*.csv"))
 full_df = pd.concat(pd.read_csv(p, low_memory=False) for p in csv_paths)
 full_df.to_csv("data/combined.csv", index=False)
