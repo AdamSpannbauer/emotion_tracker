5c32e34ff8f48243cd6b46d3d8d22f83f4b3f054
AdamSpannbauer <spannbaueradam@gmail.com>
2020-05-29 07:40:17 -0400

    add data

--- combine_data.py
+++ combine_data.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 # Read and combine all relevant csvs
 csv_paths = glob.glob("data/*.csv")
-full_df = pd.concat(pd.read_csv(p) for p in csv_paths)
+full_df = pd.concat(pd.read_csv(p, low_memory=False) for p in csv_paths)
 full_df.to_csv("data/combined.csv", index=False)
